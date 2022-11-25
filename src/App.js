5e51d69d42a2b1ff260af7be775b0a0248a6556f
mmoscosa <mmoscosa@gmail.com>
2022-11-24 18:09:36 -0600

    react project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editing <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,11 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from scratch
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
