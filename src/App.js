81ce7ae68a1149784b1c99857eae79af3f10fadb
mmoscosa <mmoscosa@gmail.com>
2022-11-25 20:47:39 -0600

    header

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Real Angels</h1>
         {isAuthenticated ? (
           <>
             <Profile />
