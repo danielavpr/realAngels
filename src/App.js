115feeb3074d655a8a5256d90bafa9b323f2bc2b
mmoscosa <mmoscosa@gmail.com>
2022-11-25 21:49:56 -0600

    removing env

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Real Angels - {process.env.NODE_ENV}</h1>
+        <h1>Real Angels</h1>
         {isAuthenticated ? (
           <>
             <Profile />
