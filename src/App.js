c3bb30d1b2be4eb096a6e6c6169eacdc60ef7add
danielavpr <daniela@soynobe.com>
2022-12-22 08:28:43 -0600

    refactor: move files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages'
-import Onboarding from './pages/onboarding/onboarding'
+import Onboarding from './pages/onboarding'
 
 import { useAuth0 } from '@auth0/auth0-react'
 import Footer from './components/Footer'
---
+++ src/pages/onboarding.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import CardButton from '../components/Common/CardButton'
+import Navbar from '../components/Landing/Navbar'
+import Sidebar from '../components/Landing/Sidebar'
+import { OnboardingContainer, OnboardingStepContainer, OnboardintStepLeftSection, OnboardingStepRightSection, OnboardingCardContainer } from '../components/Onboarding/OnboardingElements'
+import OnboardingStepWrapper from '../components/Onboarding/OnboardingStepWrapper'
+
+const Onboarding = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <OnboardingContainer>
+      <OnboardingStepContainer>
+      <OnboardintStepLeftSection>
+        <OnboardingStepWrapper title="Are you an angel or a founder?" description="We'll connect you with other profiles based on your preferences">
+          <OnboardingCardContainer>
+            <CardButton title="Angel" description="I help startups" />      
+            <CardButton title="Founder" description="I'm part of a startup" />
+          </OnboardingCardContainer>
+        </OnboardingStepWrapper>
+      </OnboardintStepLeftSection>
+      <OnboardingStepRightSection>
+        <h2>We foster the innovation in Mexico</h2>
+        <p>Connect with the people that is changing the industry 🚀</p>
+      </OnboardingStepRightSection>
+      </OnboardingStepContainer>
+      </OnboardingContainer>
+    </div>
+  )
+}
+
+export default Onboarding
