987d58a22049e14da67577517a224f013f00bbab
Daniela R <daniela.ramon12@gmail.com>
2023-04-07 19:14:10 -0600

    ♻️ update file name

--- src/pages/onboarding.js
+++ src/pages/onboarding.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import CardButton from '../components/Common/CardButton'
 import Navbar from '../components/Landing/Navbar'
 import Sidebar from '../components/Landing/Sidebar'
-import { OnboardingContainer, OnboardingStepContainer, OnboardintStepLeftSection, OnboardingStepRightSection, OnboardingCardContainer } from '../components/Onboarding/OnboardingElements'
+import { OnboardingContainer, OnboardingStepContainer, OnboardingStepLeftSection, OnboardingStepRightSection, OnboardingCardContainer } from '../components/Onboarding/OnboardingElements'
 import OnboardingStepWrapper from '../components/Onboarding/OnboardingStepWrapper'
 
 const Onboarding = () => {
@@ -17,14 +17,14 @@ const Onboarding = () => {
       <Navbar toggle={toggle} />
       <OnboardingContainer>
       <OnboardingStepContainer>
-      <OnboardintStepLeftSection>
+      <OnboardingStepLeftSection>
         <OnboardingStepWrapper title="Are you an angel or a founder?" description="We'll connect you with other profiles based on your preferences">
           <OnboardingCardContainer>
             <CardButton title="Angel" description="I help startups" />      
             <CardButton title="Founder" description="I'm part of a startup" />
           </OnboardingCardContainer>
         </OnboardingStepWrapper>
-      </OnboardintStepLeftSection>
+      </OnboardingStepLeftSection>
       <OnboardingStepRightSection>
         <h2>We foster the innovation in Mexico</h2>
         <p>Connect with the people that is changing the industry</p>
